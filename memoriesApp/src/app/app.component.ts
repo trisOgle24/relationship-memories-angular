76950c0d553918ce25a895fa773359973e118933
Tristan Ogle <tristanogle1@gmail.com>
2024-03-29 23:57:13 +0200

    Adds cards data

--- memoriesApp/src/app/app.component.ts
+++ memoriesApp/src/app/app.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'memoriesApp';
+
+  cards = [
+    {
+      titles: "Tristan's Honours Graduation",
+      subtitle: "26 April 12:25",
+      imageSrc: "../assets/Picture1.jpg",
+      imageAlt: "Photo of a Shiba Inu",
+      paragraph: "A day when you made me feel very special and handsome by taking many pictures and videos of me. I got a cool boyfriend mug on this day :)"
+    },
+    {
+      titles: "Our Children's First Birthday",
+      subtitle: "16 March 17:03",
+      imageSrc: "../assets/Picture2.JPG",
+      imageAlt: "Photo of a Shiba Inu",
+      paragraph: "I think this picture is really good representation of how the day went. A lot of struggles to get the hats on, but a lot of laughs that came with the struggles. Thank you for loving the doggies as much as you do <3"
+    }
+  ]
 }
--- memoriesApp/src/app/app.module.ts
+++ memoriesApp/src/app/app.module.ts
@@ -3,6 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatListModule} from '@angular/material/list';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCommonModule} from '@angular/material/core';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
 
 @NgModule({
   declarations: [
@@ -10,7 +22,19 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatListModule,
+    MatCardModule,
+    MatButtonModule,
+    MatCommonModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatExpansionModule,
+    MatSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
